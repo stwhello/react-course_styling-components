f23574fa7593f2ab712579bb25bf830541c0e135
stwhello <sherinpaul2001@gmail.com>
2026-02-13 13:15:52 +0530

    day 1 of building this project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,12 @@
-import logo from "../assets/logo.png";
-import "./Header.css";
+import logo from '../assets/logo.png';
+import "./Header.css"
 
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt='A canvas' />
+      <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p
-        style={{
-          color: "#a39191",
-          margin: 0,
-          textAlign: "center",
-        }}
-      >
-        A community of artists and art-lovers.
-      </p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
